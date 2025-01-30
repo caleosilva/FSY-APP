ee77e682c98aa6184bc736cc179c57dcb86cd8d6
caleosilva <caleosilva75@gmail.com>
2025-01-30 12:26:05 -0300

    feat: DialogActivity Content

--- components/formatedComponents/DialogActivity.tsx
+++ components/formatedComponents/DialogActivity.tsx
@@ -2,11 +2,18 @@ import { Separator } from "@/components/ui/separator"
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+import FormFieldView from "./FormFieldView";
 
 interface KeyPoint {
     title?: string;
@@ -32,6 +39,7 @@ export default function DialogActivity({
     activity,
     backgroundColor
 }: DialogActivityProps) {
+    console.log(activity)
     return (
         <Dialog>
             <DialogTrigger>
@@ -53,13 +61,38 @@ export default function DialogActivity({
                 </div>
 
             </DialogTrigger>
-            <DialogContent className="">
-                <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
-                    </DialogDescription>
+            <DialogContent className="w-full mx-auto">
+                <DialogHeader className="items-start gap-y-2">
+                    <DialogTitle className="">{activity?.name}</DialogTitle>
+
+                    <div className="w-full grid grid-cols-2 gap-8">
+                        <FormFieldView label="Local" placeholder={`${activity?.location}`} className=" " />
+                        <FormFieldView label="Horário" placeholder={`${activity?.time}`} />
+                    </div>
+
+                    <Separator />
+
+                    <div className="w-full max-h-[80%] overflow-y-auto">
+                        <Accordion type="single" collapsible>
+                            {activity?.key_points.map((point, index) => (
+                                <AccordionItem key={index} value={`item-${index}`}>
+                                    <AccordionTrigger>{point.title ? point.title : "Precisa ser feito"}</AccordionTrigger>
+                                    <AccordionContent className="text-left">
+                                        <p>{point.details}</p>
+                                        {point.todo && point.todo.length > 0 && (
+                                            <div className="mt-2">
+                                                <ul className="list-disc pl-5">
+                                                    {point.todo.map((task, taskIndex) => (
+                                                        <li key={taskIndex}>{task}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </AccordionContent>
+                                </AccordionItem>
+                            ))}
+                        </Accordion>
+                    </div>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
